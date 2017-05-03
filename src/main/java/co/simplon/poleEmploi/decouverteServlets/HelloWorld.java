e4f3efe25771da1b5d5c27c64a1d284de3cf33d2
Eric Siber <eric.siber@gmail.com>
2017-05-03 14:03:11 +0200

    Mise en place d'un MVC

--- src/main/java/co/simplon/poleEmploi/decouverteServlets/HelloWorld.java
+++ src/main/java/co/simplon/poleEmploi/decouverteServlets/HelloWorld.java
@@ -3,6 +3,7 @@ package co.simplon.poleEmploi.decouverteServlets;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -40,20 +41,8 @@ public class HelloWorld extends HttpServlet {
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		String message = messagePut;
-		String parametre = request.getParameter("nom");
-		if (parametre != null) {
-			message += parametre;
-		} else {
-			message += "World";
-		}
-
-		// Set response content type
-		response.setContentType("text/html");
-
-		// Actual logic goes here.
-		PrintWriter out = response.getWriter();
-		out.println("<h1>" + message + "</h1>");
+		RequestDispatcher rd = request.getRequestDispatcher("hello.jsp");
+		rd.forward(request, response);
 	}
 
 	public void destroy() {
